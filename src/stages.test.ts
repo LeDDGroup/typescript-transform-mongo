65654c8090f7afa5721d6ef4273c89f8f6e99a67
Daniel Perez Alvarez <danielpza@protonmail.com>
2020-04-26 19:55:26 -0400

    improve stages tests

---
+++ src/stages.test.ts
@@ -0,0 +1,62 @@
+import * as ts from "typescript";
+import { transformStage } from "./stages";
+
+const compilerOptions = {
+  target: ts.ScriptTarget.ESNext,
+  module: ts.ModuleKind.CommonJS,
+};
+
+function compileStage(stage: string, source: string) {
+  const { outputText } = ts.transpileModule(source, {
+    compilerOptions,
+    transformers: {
+      after: [
+        (context) => (node) => {
+          const firstStatement = node.statements[0];
+          console.log(ts.SyntaxKind[firstStatement.kind]);
+          if (!ts.isExpressionStatement(firstStatement)) {
+            throw new Error("Expected expression");
+          }
+          if (ts.isParenthesizedExpression(firstStatement.expression)) {
+            return ts.updateSourceFileNode(node, [
+              ts.createStatement(
+                transformStage(
+                  stage,
+                  ts.createNodeArray([firstStatement.expression.expression]),
+                  context
+                )
+              ),
+            ]);
+          }
+          return ts.updateSourceFileNode(node, [
+            ts.createStatement(
+              transformStage(
+                stage,
+                ts.createNodeArray([firstStatement.expression]),
+                context
+              )
+            ),
+          ]);
+        },
+      ],
+    },
+  });
+  return outputText;
+}
+
+function check(stage: string, source: string, expected: string) {
+  const actual = compileStage(stage, source);
+  expect(actual).toBe(expected);
+}
+
+test("property access", () => {
+  check(
+    "$addFields",
+    `\
+({ foo: this.bar });
+`,
+    `\
+({ $addFields: { foo: "$bar" } });
+`
+  );
+});
