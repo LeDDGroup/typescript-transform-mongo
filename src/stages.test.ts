0ab902b3b39517fe9b977a0cdb08abf3679073b4
Daniel Perez Alvarez <danielpza@protonmail.com>
2020-04-27 20:13:26 -0400

    todo for stages

--- src/stages.test.ts
+++ src/stages.test.ts
@@ -48,7 +48,8 @@ function check(stage: string, source: string, expected: string) {
   expect(actual).toBe(expected);
 }
 
-test("property access", () => {
+// https://docs.mongodb.com/manual/reference/operator/aggregation-pipeline/
+test("$addFields", () => {
   check(
     "$addFields",
     `\
@@ -59,3 +60,41 @@ test("property access", () => {
 `
   );
 });
+
+test.todo("$bucket");
+test.todo("$bucketAuto");
+test.todo("$collStats");
+test.todo("$count");
+test.todo("$facet");
+test.todo("$geoNear");
+test.todo("$graphLookup");
+test.todo("$group");
+test.todo("$indexStats");
+test.todo("$limit");
+test.todo("$listSessions");
+test.todo("$lookup");
+test.todo("$match");
+test.todo("$merge");
+test.todo("$out");
+test.todo("$planCacheStats");
+test.todo("$project");
+test.todo("$redact");
+test.todo("$replaceRoot");
+test.todo("$replaceWith");
+test.todo("$sample");
+test.todo("$set");
+test.todo("$skip");
+test.todo("$sort");
+test.todo("$sortByCount");
+test.todo("$unset");
+test.todo("$unwind");
+
+// https://docs.mongodb.com/manual/reference/operator/aggregation-pipeline/#db-aggregate-stages
+
+test.todo("$currentOp");
+test.todo("$listLocalSessions");
+
+// https://docs.mongodb.com/manual/reference/operator/aggregation-pipeline/#stages-available-for-updates
+
+test.todo("$findAndModify");
+test.todo("$update");
